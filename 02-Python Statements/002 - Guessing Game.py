1e1b3ab6820a7ae780be11b67d888b557563ae3c
marc.egli <marc.egli@uzh.ch>
2021-02-11 10:56:54 +0100

    Saw guessing game changes

--- 02-Python Statements/002 - Guessing Game.py
+++ 02-Python Statements/002 - Guessing Game.py
@@ -9,30 +9,30 @@
 # farther from the number than the previous guess, return "COLDER!"
 # When the player's guess equals the number, tell them they've guessed correctly and how many guesses it took!
 
+print("Would you like to play a game?\nTry to guess the number I'm thinking of. \nIt's between one and hundred")
+
 from random import randint
 
-guess  = int(input("Insert your number here: "))
+guess  = int(input("What is your first guess: "))
 x = randint(0,100)
 guesslis = []
-print(x)
 
 while guess != x:
     if guess not in range(0,101):
-        print("out of bounds")
-        guesslis.append(guess)
-        guess = int(input("Insert new guess: "))
+        print("out of bounds you idiot. I said between 1 and 100!\nI won't count it this time.") #I won't count stupid guesses
+        guess = int(input("Guess again stupido: "))
     elif len(guesslis) == 0:
         if abs(guess - x) <= 10:
-            print("Warm!")
+            print("Not correct but Warm!")
         else:
-            print("Cold!")
+            print("Not correct. Really far away! Cold!")
         guesslis.append(guess)
-        guess = int(input("Insert new guess: "))
+        guess = int(input("Guess again: "))
     else:
         if abs(guesslis[-1]-x) > abs(guess-x):
-            print("Warmer")
+            print("Yesss! Yess! Keep guessing, getting closer!")
         else:
-            print("Colder")
+            print("No, no, no you're getting further away!!!")
         guesslis.append(guess)
         guess = int(input("Insert new guess: "))
 print("Congratulations, you guessed the number")
